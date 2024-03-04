6b65bcea5a9a433441a53e804ee226601d9bb9b2
Sachin Gayakwad <sachin142322@gmail.com>
2024-03-04 16:32:05 +0530

    feature 3 added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log("Hello World");
 console.log("feature 1");
 console.log("feature 2");
+console.log("feature 3");
+
 console.log("this is feature 1");
 console.log("this is feature 2");
+console.log("this is feature 3");
+
 
